5818de767b0ec1397357505ada864a1cd9e7626b
caitlin <cabrerac@g.cofc.edu>
2020-11-26 15:55:36 -0500

    changes to service classes

--- src/main/java/business/edu/cofc/cs656/services/PostMatesServices.java
+++ src/main/java/business/edu/cofc/cs656/services/PostMatesServices.java
@@ -1,13 +1,15 @@
 package business.edu.cofc.cs656.services;
 
-import model.edu.cofc.cs656.CustomerOrder;
+import model.edu.cofc.cs656.DeliveryDriver;
 import model.edu.cofc.cs656.Review;
 
 import java.util.ArrayList;
 
 public class PostMatesServices {
 
-    static ArrayList<Review> lst = new ArrayList<>();
+    private static ArrayList<Review> lst = new ArrayList<>();
+
+
 
 
     public static ArrayList<Review> deleteReview(ArrayList<Review> r1) {
@@ -21,6 +23,52 @@ public class PostMatesServices {
         return lst;
 
     }
+
+    public static ArrayList<DeliveryDriver> createDeliveryDriver() {
+
+        ArrayList<DeliveryDriver> dlst = new ArrayList<>();
+        DeliveryDriver d1 = new DeliveryDriver("Linda Belcher", "333-333-3333", "tina@aol.com", "4503 2nd St", "password", "DRIVERID_+6466464");
+        dlst.add(d1);
+
+
+        return dlst;
+
+    }
+
+    public static ArrayList<DeliveryDriver> addDeliveryDriver() {
+
+        ArrayList<DeliveryDriver> dlst = new ArrayList<>();
+        DeliveryDriver d1 = new DeliveryDriver("Bob Belcher", "333-333-3333", "tina@aol.com", "4503 2nd St", "password", "DRIVERID_+6466464");
+        dlst.add(d1);
+
+
+        return dlst;
+
+    }
+
+    public static ArrayList<DeliveryDriver> viewDeliveryDriver() {
+
+        ArrayList<DeliveryDriver> dlst = new ArrayList<>();
+        DeliveryDriver d1 = new DeliveryDriver("Bob Belcher", "333-333-3333", "tina@aol.com", "4503 2nd St", "password", "DRIVERID_+6466464");
+        dlst.add(d1);
+
+
+        return dlst;
+
+    }
+
+    public static ArrayList<DeliveryDriver> deleteDeliveryDriver() {
+
+        ArrayList<DeliveryDriver> dlst = new ArrayList<>();
+        DeliveryDriver d1 = new DeliveryDriver("Bob Belcher", "333-333-3333", "tina@aol.com", "4503 2nd St", "password", "DRIVERID_+6466464");
+        dlst.remove(d1);
+
+
+        return dlst;
+
+    }
+
+
 
 
 
